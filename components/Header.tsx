20a7a5affd25480a37f7ee343137e390a880fe39
ghamry <ghamry648@gmail.com>
2023-08-14 21:31:15 +0300

    google auth added

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,18 +27,25 @@ export default function Header() {
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
           href={"api/auth/signin"}
           rel="noopener noreferrer">
-          <Github />
-          <p>GitHub Sign In</p>
+
+          <p>Sign In</p>
         </Link>
       )}
       {session && (
-        <Link
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
-          href={"/"}
-          onClick={() => signOut()}
-          rel="noopener noreferrer">
-          <p>Sign Out</p>
-        </Link>
+        <div className="flex max-w-fit items-center justify-items-center space-x-2 ">
+          <p>welcome {session.user?.name}</p>
+          {session.user?.image && (
+            <Image width="40" height="40" alt="avatar" src={session.user?.image} className=" rounded-sm" />
+          )}
+          <Link
+            className="rounded-full border text-white px-5 py-2 text-sm shadow-md hover:bg-gray-500 bg-black-600 font-medium transition"
+            href={"/"}
+            onClick={() => signOut()}
+            rel="noopener noreferrer">
+            <p>Sign Out</p>
+          </Link>
+
+        </div>
       )}
 
     </header>
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -2,14 +2,19 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-
+import GoogleProvider from "next-auth/providers/google";
 const prisma = new PrismaClient();
 
 export default NextAuth({
     providers: [GitHubProvider({
         clientId: process.env.GITHUB_ID ?? "",
         clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string,
     }),],
+    secret: process.env.NEXTAUTH_SECRET,
     adapter: PrismaAdapter(prisma),
 });
 
