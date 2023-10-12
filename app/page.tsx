18f3271f0412bb095b020e26c01c4f363eb8c50f
Mohamed Elghamry <melghamry.c@stcpay.com.sa>
2023-10-12 15:41:52 +0300

    should i buy

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,44 @@
-import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SquigglyLines from "../components/SquigglyLines";
 import { Metadata } from "next";
-let title = "Dream Room Generator";
-let description = "Generate your dream room in seconds.";
-let ogimage = "https://roomgpt-demo.vercel.app/og-image.png";
-let sitename = "roomGPT.io";
+let title = "Should I Buy ?";
+let description = "fin the best deal in seconds.";
+
 
 export const metadata: Metadata = {
   title,
   description,
   icons: {
-    icon: "/favicon.ico",
-  },
-  openGraph: {
-    images: [ogimage],
-    title,
-    description,
-    url: "https://roomgpt-demo.vercel.app",
-    siteName: sitename,
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [ogimage],
-    title,
-    description,
-  },
+    icon: "/fav.ico",
+  }
 };
 export default function HomePage() {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
-        {/* <Link
-        //   href="https://vercel.fyi/roomGPT"
-        //   target="_blank"
-        //   rel="noreferrer"
-        //   className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
-        // >
-        //   Clone and deploy your own with{" "}
-        //   <span className="text-blue-600">Vercel</span>
-        // </Link> */}
+
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
+          Your one-stop destination for effortless and efficient product searches. {" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">Should I Buy !</span>
           </span>{" "}
           for everyone.
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-          Take a picture of your room and see how your room looks in different
-          themes. 100% free – remodel your room today.
+          just type the product name
         </h2>
+        <input type="text" ></input>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Generate your dream room
+          Search
         </Link>
-        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+        {/* <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
               <div>
@@ -88,7 +63,7 @@ export default function HomePage() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </main>
       <Footer />
     </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -13,13 +13,13 @@ export default function Header() {
       <Link href="/" className="flex space-x-2">
         <Image
           alt="header text"
-          src="/bed.svg"
+          src="/fav.svg"
           className="sm:w-10 sm:h-10 w-9 h-9"
           width={24}
           height={24}
         />
         <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
-          roomGPT.io
+          Should I Buy !
         </h1>
       </Link>
       {!session && (
