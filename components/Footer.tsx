bc73d9c7fd4c4b73ac2701085fe32216c0b0499a
ghamry <ghamry648@gmail.com>
2023-10-16 20:32:22 +0300

    footer enhance

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3 border-gray-500">
       <div className="text-gray-500">
-        Made with love by{" "}
+        Made with love ❤️ by{" "}
         <Link
           href="https://www.linkedin.com/in/ghamry"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition hover:text-gray-300 underline-offset-2"
         >
-          Ghamry,{" "}
+          Ghamry{" "}
         </Link>
 
       </div>
